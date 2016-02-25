fd8f259b3a7ae9f2eaec765a7c909e093a982b8f
Matthew Duffy <mattduffy@gmail.com>
2016-02-25 15:16:04 -0800

    Move work over to imac.

---
+++ app/index.js
@@ -0,0 +1,15 @@
+'use strict';
+const router = require('express').Router();
+
+router.get('/', (req, res, next)=>{
+  res.render('login', {
+    pageTitle: "My Login Page"
+  });
+})
+
+router.get('/info', (req, res, next)=>{
+  res.send('test page');
+})
+module.exports = {
+  router: router
+}
--- server.js
+++ server.js
@@ -1,3 +1,19 @@
 'use strict';
+const express = require('express'),
+  app = express(),
+  chatcat = require('./app');
 
-console.log("Welcome to fapping cats chatting about loose chads.");
+let port = process.env.PORT || 3000;
+app.set('port', port);
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+// register first of possibly many sub app routers
+// this is mounted at root /
+app.use('/', chatcat.router);
+//app.use('/dashboard', dashboard.router);
+//app.use('/api', api.router);
+
+app.listen(port, ()=>{
+  console.log("Chat cat listening on port ", port);
+});
